87446773818d10297224adb9b1cbb1934fc8112e
juandeto <juandeto10@gmail.com>
2020-11-13 09:19:16 -0300

    changes in cors

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ require('dotenv').config();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors())
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nookdeco.com.ar"); 
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+	origin: '*',
+	methods: ['GET', 'POST', 'PATCH'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+})
+);
 app.use(express.static(__dirname + '/public'))
 
 // Index Route
