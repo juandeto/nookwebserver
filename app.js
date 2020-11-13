1b802b6469399c1a6ce4eac4f8af45b6538556dc
juandeto <juandeto10@gmail.com>
2020-11-13 19:52:50 -0300

    changes in cors@

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ require('dotenv').config();
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin");
+	next();
+});
 app.use(express.static(__dirname + '/public'))
 
 // Index Route
