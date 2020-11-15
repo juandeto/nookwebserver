7ed149acd299fb5126e2d61b629363493b1c38f9
juandeto <juandeto10@gmail.com>
2020-11-14 22:44:32 -0300

    add mail secure

--- routes/emails.js
+++ routes/emails.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
     // Cambiar los datos según mail que se utilizará para el manejo de emails.
     // Yo utilicé este que es para hacer pruebas, se pueden ver la pruebas en https://ethereal.email/messages/5fa37f38ccbc44346152dc1e/22
     host: 'smtp.mail.gmail.com',
+    secure: true,
     port: 465,
     auth: {
         // Se setean en archivo .env
